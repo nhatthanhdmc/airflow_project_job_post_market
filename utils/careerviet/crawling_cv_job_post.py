2f7d3e03b153d543c686be51e5197fd13bc48ef0
nhatthanhdmc <nhatthanhdmc@gmail.com>
2024-08-08 21:48:30 +0700

    update count

--- utils/careerviet/crawling_cv_job_post.py
+++ utils/careerviet/crawling_cv_job_post.py
@@ -407,7 +407,7 @@ def job_url_generator_airflow(worker):
         print(document["job_url"])
         crawl_job_post_worker(document["job_url"]) 
         count += 1
-        if  count > 100:
+        if  count > 1000:
             break
         #     break   
     # Close the connection    
